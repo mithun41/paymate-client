f3ea9c465c2bfeb2962e772f2321d8ed5254ef4a
mithun8441 <mahmudulhasan3527@gmail.com>
2025-09-13 21:18:14 +0600

    added a p tag

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,7 +1,16 @@
 import React from "react";
 
 const Home = () => {
-  return <div>This is Home.</div>;
+  return (
+    <div>
+      This is Home.
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores
+        veritatis a itaque enim omnis aliquam vel animi assumenda corporis vero
+        cumque quod ex et quis iste harum, voluptas numquam sunt?
+      </p>
+    </div>
+  );
 };
 
 export default Home;
