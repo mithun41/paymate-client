f971b123b4b1c8af05a30d0f97a1202df229ac0f
mithun8441 <mahmudulhasan3527@gmail.com>
2025-09-13 22:37:16 +0600

    conflict

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 
 const Home = () => {
-  return <div>
-    <p>this is home component</p>
-    <p>Sheauly</p>
-  </div>;
-
+  return (
+    <div>
+      <p>this is home component</p>
+      <p>Sheauly</p>
+      <h2>heelo</h2>
+    </div>
+  );
 };
 
 export default Home;
