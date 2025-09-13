5f0385d734fcebbdf1be702d91c31cde04d08240
mithun8441 <mahmudulhasan3527@gmail.com>
2025-09-13 21:20:10 +0600

    added a h

--- src/Pages/Home/Home/Home.jsx
+++ src/Pages/Home/Home/Home.jsx
@@ -9,6 +9,7 @@ const Home = () => {
         veritatis a itaque enim omnis aliquam vel animi assumenda corporis vero
         cumque quod ex et quis iste harum, voluptas numquam sunt?
       </p>
+      <h1>hello word</h1>
     </div>
   );
 };
