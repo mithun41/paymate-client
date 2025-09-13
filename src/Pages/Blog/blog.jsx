af09d4581fc4c0647f0ae006cacdc811f2dbd28b
mhsumon0174 <mhsumon0174@gmail.com>
2025-09-13 21:30:32 +0600

    sumon's first commit

--- src/Pages/Blog/blog.jsx
+++ src/Pages/Blog/blog.jsx
@@ -4,6 +4,7 @@ const blog = () => {
     return (
         <div>
             <h1>This is blog post</h1>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Cupiditate, ea sint minima esse modi deserunt quas aliquam ad reprehenderit voluptatum quaerat impedit est ut! Deserunt similique ab quis eaque repellat!</p>
         </div>
     );
 };
